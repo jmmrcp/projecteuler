38e576e1b89ea715b0705c8625d45e45293920f4
jmmrcp <jmmrcp@gmail.com>
2016-03-02 18:08:34 +0100

    Comentarios en main

--- 001/main.go
+++ 001/main.go
@@ -1,3 +1,6 @@
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.*/
+
 package main
 
 import "fmt"
@@ -10,5 +13,5 @@ func main() {
 			suma += i
 		} // End if
 	} // End for
-	fmt.Printf("La suma de los numeros es: %d.\n", suma)
+	fmt.Printf("La suma de los numeros es: %d.\n", suma) // 233168
 }
