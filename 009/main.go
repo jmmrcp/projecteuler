ad989f2e5d86978f04c215b05075c88d0b2774bb
jmmrcpsip <jmmrcpsip@gmail.com>
2016-03-03 09:43:31 +0100

    009

--- 009/main.go
+++ 009/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := 0
+	b := 0
+	c := 0
+	s := 1000
+	found := false
+	for a = 1; a < s/3; a++ {
+		for b = a; b < s/2; b++ {
+			c = s - a - b
+			if (a*a)+(b*b) == (c * c) {
+				found = true
+				break
+			}
+		}
+		if found == true {
+			break
+		}
+	}
+	fmt.Printf("Los numeros son: %+v, %+v, %+v.\n", a, b, c)
+	fmt.Printf("Producto de abc: %v.\n", a*b*c) // 31875000
 }
